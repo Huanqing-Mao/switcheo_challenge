2187d6cc10c4bddf5426db917d74f958a0c53591
Huanqing-Mao <122110454+Huanqing-Mao@users.noreply.github.com>
2024-02-19 14:42:30 +0800

    Adding Problem 3 files in one folder
    - Original file contains original code and my comments on the issue
    - The improved file contains the improved code and my implementation of the class

---
+++ src/Problem3/Problem3_original.tsx
@@ -0,0 +1,108 @@
+import React, { useState, useEffect, useMemo } from "react";
+// It is unknown of what libraries are used. Here I assume that all the functions are pre-defined or passed in as props.
+
+interface WalletBalance {
+  currency: string;
+  amount: number;
+  // There is no blockchain field in the wallet balance. Hence, later we cannot use walletbalance.blockchain
+  // include blockchain field
+}
+
+// The use of FormattedWalletBalance is redundant, because you can just pass the formatted string to the WalletBalance when you create the WalletBalance object
+interface FormattedWalletBalance {
+  currency: string;
+  amount: number;
+  formatted: string;
+}
+
+class Datasource {
+  // TODO: Implement datasource class
+}
+
+interface Props extends BoxProps {}
+const WalletPage: React.FC<Props> = (props: Props) => {
+  const { children, ...rest } = props;
+  const balances = useWalletBalances(); // the naming is confusing, it should be getWalletBalances()
+  const [prices, setPrices] = useState({});
+
+  useEffect(() => {
+    const datasource = new Datasource(
+      "https://interview.switcheo.com/prices.json"
+    );
+    datasource
+      .getPrices()
+      .then((prices) => {
+        setPrices(prices);
+      })
+      .catch((error) => {
+        // it should be console.error(error)
+        console.err(error);
+      });
+  }, []);
+
+  const getPriority = (blockchain: any): number => {
+    switch (blockchain) {
+      case "Osmosis":
+        return 100;
+      case "Ethereum":
+        return 50;
+      case "Arbitrum":
+        return 30;
+      case "Zilliqa":
+        return 20;
+      case "Neo":
+        return 20;
+      default:
+        return -99;
+    }
+  };
+
+  const sortedBalances = useMemo(() => {
+    return balances
+      .filter((balance: WalletBalance) => {
+        const balancePriority = getPriority(balance.blockchain);
+        // balancePriority is not used, lhsPriority is not defined. Hence it is very unclear.
+        // I assume balances is filtered by balancePriority.
+        if (lhsPriority > -99) {
+          if (balance.amount <= 0) {
+            return true;
+          }
+        }
+        return false;
+      })
+      .sort((lhs: WalletBalance, rhs: WalletBalance) => {
+        const leftPriority = getPriority(lhs.blockchain);
+        const rightPriority = getPriority(rhs.blockchain);
+        if (leftPriority > rightPriority) {
+          return -1;
+        } else if (rightPriority > leftPriority) {
+          return 1;
+        } // here misses an else statement which returns 0 to handle the case where the priority scores are the same
+      });
+  }, [balances, prices]);
+
+  const formattedBalances = sortedBalances.map((balance: WalletBalance) => {
+    return {
+      ...balance,
+      formatted: balance.amount.toFixed(),
+    };
+  });
+
+  // It should be formattedBalances.map(...) instead of using sortedBalances again
+  const rows = sortedBalances.map(
+    (balance: FormattedWalletBalance, index: number) => {
+      const usdValue = prices[balance.currency] * balance.amount;
+      return (
+        <WalletRow
+          className={classes.row} // Assume that classes is passed in through props
+          key={index}
+          amount={balance.amount}
+          usdValue={usdValue}
+          formattedAmount={balance.formatted}
+        />
+      );
+    }
+  );
+
+  return <div {...rest}>{rows}</div>;
+};
