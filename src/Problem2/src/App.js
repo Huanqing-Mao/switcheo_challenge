2aedeff53c3f57dba2c558fd06b02e01077dc758
Huanqing-Mao <122110454+Huanqing-Mao@users.noreply.github.com>
2024-02-19 22:04:00 +0800

    Update Problem 2

--- src/Problem2/src/App.js
+++ src/Problem2/src/App.js
@@ -3,15 +3,12 @@ import React from "react";
 import Banner from "./Components/Banner";
 import { Divider } from "antd";
 import SwapBarContainer from "./Components/SwapBarContainer";
+import Template from "./Components/Template";
 
 export default function App() {
   return (
     <div className="App">
-      <Banner />
-      <Divider />
-      <div className="main-page">
-        <SwapBarContainer />
-      </div>
+      <Template />
     </div>
   );
 }
